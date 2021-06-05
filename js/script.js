3ff510e26dcc85218585d6d663f15bc62096b1ba
dienoov <alvinodien@yahoo.co.id>
2021-06-05 12:19:09 +0700

    about

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+const nav = document.querySelector("nav");
+
+document.addEventListener("scroll", () => {
+    scrollY > 500 ?
+        nav.classList.add("scrolled") :
+        nav.classList.remove("scrolled");
+});
+
 const slider = document.getElementById("slider");
 const sliderArticles = slider.querySelectorAll("article");
 const sliderIndicators = slider.querySelectorAll(".indicators li");
 
-const slideTo = (index) => {
+let sliderIndex = sliderArticles.length;
+
+const sliderSlideTo = (index) => {
+    sliderIndex = index;
+
     sliderArticles.forEach((article) => {
         article.classList.remove("active");
     });
@@ -17,13 +29,45 @@ const slideTo = (index) => {
 sliderIndicators.forEach((indicator) => {
     indicator.addEventListener("click", (ev) => {
         const index = ev.target.dataset.index;
-        slideTo(index);
+        sliderSlideTo(index);
     });
 });
 
-let index = sliderArticles.length;
 setInterval(() => {
-    index++;
-    index = index >= sliderArticles.length ? 0 : index;
-    slideTo(index);
+    sliderIndex++;
+    sliderIndex = sliderIndex >= sliderArticles.length ? 0 : sliderIndex;
+    sliderSlideTo(sliderIndex);
+}, 5000);
+
+const about = document.getElementById("about");
+const aboutImages = about.querySelectorAll("img");
+const aboutIndicators = about.querySelectorAll(".indicators li");
+
+let aboutIndex = aboutImages.length;
+
+const aboutSlideTo = (index) => {
+    aboutIndex = index;
+
+    aboutImages.forEach((article) => {
+        article.classList.remove("active");
+    });
+    aboutIndicators.forEach((indicator) => {
+        indicator.classList.remove("active");
+    });
+
+    aboutImages[index].classList.add("active");
+    aboutIndicators[index].classList.add("active");
+};
+
+aboutIndicators.forEach((indicator) => {
+    indicator.addEventListener("click", (ev) => {
+        const index = ev.target.dataset.index;
+        aboutSlideTo(index);
+    });
+});
+
+setInterval(() => {
+    aboutIndex++;
+    aboutIndex = aboutIndex >= aboutImages.length ? 0 : aboutIndex;
+    aboutSlideTo(aboutIndex);
 }, 5000);
